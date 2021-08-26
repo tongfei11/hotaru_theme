5485b64b75fc09f3009e6203aafde48237fc9d10
cokemine <aptx4561@gmail.com>
2021-08-26 19:00:56 +0800

    chore: update dependency

--- types/servers.d.ts
+++ types/servers.d.ts
@@ -1,42 +1,39 @@
-/* eslint-disable */
-declare global {
-  interface BoxItem {
-    id: number;
-    name: string;
-    host: string;
-    type: string;
-    location: string;
-    region: string;
-    status: Record<string, never>;
-  }
-
-  interface StatusItem extends BoxItem {
-    status: {
-      online4: boolean;
-      online6: boolean;
-      uptime: number;
-      load: number;
-      cpu: number;
-      network_rx: number;
-      network_tx: number;
-      network_in: number;
-      network_out: number;
-      memory_total: number;
-      memory_used: number;
-      swap_total: number;
-      swap_used: number;
-      hdd_total: number;
-      hdd_used: number;
-      custom: string;
-    };
-  }
-
-  interface Window {
-    __PRE_CONFIG__: {
-      header: string;
-      subHeader: string;
-      footer: string;
-    };
-  }
+interface BoxItem {
+  id: number;
+  name: string;
+  host: string;
+  type: string;
+  location: string;
+  region: string;
+  status: Record<string, never>;
 }
-export {};
+
+interface StatusItem extends BoxItem {
+  status: {
+    online4: boolean;
+    online6: boolean;
+    uptime: number;
+    load: number;
+    cpu: number;
+    network_rx: number;
+    network_tx: number;
+    network_in: number;
+    network_out: number;
+    memory_total: number;
+    memory_used: number;
+    swap_total: number;
+    swap_used: number;
+    hdd_total: number;
+    hdd_used: number;
+    custom: string;
+  };
+}
+
+interface Window {
+  __PRE_CONFIG__: {
+    header: string;
+    subHeader: string;
+    footer: string;
+  };
+}
+
